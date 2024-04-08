c15c0161d26688ff7dba7090bb8c6274dddd51d8
jules-guillot <juloss43@gmail.com>
2024-04-08 14:53:53 +0200

    Partie graphique améliorée

--- app/src/main/java/fr/isen/guillot/androidsmartdevice/MainActivity.kt
+++ app/src/main/java/fr/isen/guillot/androidsmartdevice/MainActivity.kt
@@ -5,14 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import android.content.Intent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -32,78 +30,58 @@ val AppTypography = Typography(
         letterSpacing = 0.sp
     )
 )
-
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val backgroundColor = colorResource(id = R.color.bleu_cyan)
+            val topbartext = colorResource(id = R.color.vert)
+            val topbarbackgroundColor = colorResource(id = R.color.bleu_fonce)
+            val context = LocalContext.current
+            val boutontext = colorResource(id = R.color.bleu_clair)
             MaterialTheme(
                 typography = AppTypography
             ) {
-                Box(
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = "Android Smart Device",
+                            color = topbartext,
+                            style = MaterialTheme.typography.displayLarge
+                        ) },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = topbarbackgroundColor
+                    )
+                )
+                Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(color = backgroundColor)
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    MainScreen()
+                    Image(painter = painterResource(id = R.drawable.icon), contentDescription = "Icone App")
+                    Spacer(modifier = Modifier.height(90.dp))
+                    Button(
+                        onClick = {
+                            val intent = Intent(context, ScanActivity::class.java)
+                            context.startActivity(intent)
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 120.dp)
+                            .height(70.dp),
+                        shape = MaterialTheme.shapes.medium,
+                        colors = ButtonDefaults.buttonColors(topbarbackgroundColor)
+                    ) {
+                        Text(
+                            text = "Scanner",
+                            color = boutontext,
+                            style = MaterialTheme.typography.bodyLarge,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
                 }
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen() {
-    val context = LocalContext.current
-    val topbarbackgroundColor = colorResource(id = R.color.bleu_fonce)
-    val boutontext = colorResource(id = R.color.bleu_clair)
-    val topbartext = colorResource(id = R.color.vert)
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "Android Smart Device",
-                        color = topbartext,
-                        style = MaterialTheme.typography.displayLarge
-
-                    ) },
-                    colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = topbarbackgroundColor
-                )
-            )
-        }
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(painter = painterResource(id = R.drawable.icon), contentDescription = "Icone App")
-            Spacer(modifier = Modifier.height(90.dp))
-            Button(
-                onClick = {
-                    val intent = Intent(context, ScanActivity::class.java)
-                    context.startActivity(intent)
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 120.dp)
-                    .height(70.dp),
-                shape = MaterialTheme.shapes.medium,
-                colors = ButtonDefaults.buttonColors(topbarbackgroundColor)
-            ) {
-                Text(
-                    text = "Scanner",
-                    color = boutontext,
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.fillMaxWidth()
-                )
             }
         }
     }
--- app/src/main/java/fr/isen/guillot/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/fr/isen/guillot/androidsmartdevice/ScanActivity.kt
@@ -3,22 +3,51 @@ package fr.isen.guillot.androidsmartdevice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 class ScanActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme(typography = AppTypography) {
-                ScanScreen() // Affichez l'UI de scan ici
+            val topbartext = colorResource(id = R.color.vert)
+            val topbarbackgroundColor = colorResource(id = R.color.bleu_fonce)
+            MaterialTheme(
+                typography = AppTypography
+            ) {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = "   Scanner Bluetooth",
+                            color = topbartext,
+                            style = MaterialTheme.typography.displayLarge
+                        ) },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = topbarbackgroundColor
+                    )
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                }
             }
         }
     }
 }
 
-@Composable
-fun ScanScreen() {
-    Text(text = "Interface de scan BLE")
-}
