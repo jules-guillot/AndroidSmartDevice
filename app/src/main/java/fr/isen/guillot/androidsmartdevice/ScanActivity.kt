900ecb5bfc3b44813d0eedaaa08e91d4fb6d52d5
jules-guillot <juloss43@gmail.com>
2024-04-12 11:48:17 +0200

    Liste des appareils fictifs

--- app/src/main/java/fr/isen/guillot/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/fr/isen/guillot/androidsmartdevice/ScanActivity.kt
@@ -1,5 +1,6 @@
 package fr.isen.guillot.androidsmartdevice
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,12 +10,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +36,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.*
 @OptIn(ExperimentalMaterial3Api::class)
 class ScanActivity : ComponentActivity() {
+    @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -39,6 +45,18 @@ class ScanActivity : ComponentActivity() {
             val topbarbackgroundColor = colorResource(id = R.color.bleu_fonce)
             val topscannerBLE = colorResource(id = R.color.bleu_fonce_scan_activity)
             val lignedechargement = colorResource(id = R.color.white)
+
+            val devices = remember { mutableStateListOf<BluetoothDevice>() }
+
+            LaunchedEffect(Unit) {
+                devices.add(BluetoothDevice("La tv", "00:11:22:33:44:55", R.drawable.fort))
+                devices.add(BluetoothDevice("La brosse à dent", "00:12:22:33:44:55", R.drawable.faible))
+                devices.add(BluetoothDevice("Le séche cheveux", "00:15:22:33:44:55", R.drawable.moyen))
+                devices.add(BluetoothDevice("La PS6", "00:11:22:33:47:55", R.drawable.faible))
+                devices.add(BluetoothDevice("L'écran plat de la cuisine", "01:11:22:33:44:55", R.drawable.moyen))
+                devices.add(BluetoothDevice("Thermomix", "00:11:22:33:44:58", R.drawable.fort))
+            }
+
             MaterialTheme(
                 typography = AppTypography
             ) {
@@ -80,6 +98,31 @@ class ScanActivity : ComponentActivity() {
                         }
                     }
                 }
+                if (firstPhotoVisible) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 10.dp, vertical = 2000.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.icon),
+                            contentDescription = "Icone App"
+                        )
+                    }
+                } 
+                else {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 10.dp, vertical = 200.dp)
+                    ) {
+                        LazyColumn {
+                            items(devices) { device ->
+                                DeviceItem(device)
+                            }
+                        }
+                    }
+                }
                 TopAppBar(
                     title = {
                         Text(
@@ -100,6 +143,28 @@ class ScanActivity : ComponentActivity() {
                 ) {
                 }
             }
+        }
+    }
+}
+data class BluetoothDevice(val name: String, val macAddress: String, val imageResourceId: Int)
+
+@Composable
+fun DeviceItem(device: BluetoothDevice) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            painter = painterResource(id = device.imageResourceId),
+            contentDescription = "Bluetooth Device",
+            modifier = Modifier
+                .size(75.dp)
+        )
+        Column {
+            Text(text = "   ", style = MaterialTheme.typography.bodySmall)
+            Text(text = "   " + device.name, style = MaterialTheme.typography.bodyLarge)
+            Text(text = "           " + device.macAddress, style = MaterialTheme.typography.bodySmall)
         }
     }
 }
